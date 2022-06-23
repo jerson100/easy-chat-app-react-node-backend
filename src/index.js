524a1993f23a925b061026998be6448a337a805f
jerson100 <juamkoo@gmail.com>
2022-06-23 18:02:28 -0500

    add socketio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("login", (us) => {});
+  socket.on("addComment", (comment) => {
+    console.log(comment);
+    socket.emit("newComment", comment);
+  });
+});
 
 app.get("/", (req, res) => {
   return res.json({
@@ -8,7 +24,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3100, () => {
+server.listen(process.env.PORT || 3100, () => {
   console.log(
     `Servidor est{a escuchando en el puerto ${process.env.PORT || 3100}`
   );
